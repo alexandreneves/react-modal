d03280a10542fb4200a8593a8b90b420062e2cdf
Alexandre Neves <aneves@grond.home>
2016-11-09 23:10:04 +0000

    removed unused class

--- example/src/components/App.js
+++ example/src/components/App.js
@@ -10,7 +10,7 @@ let App = ({dispatch}) => {
         <div>
             <div className="examples">
                 <h3>example</h3>
-                <button type="button" className="button-primary" onClick={e => {dispatch(modalToggle('modal-foobar'))}}>try me</button>
+                <button type="button" onClick={e => {dispatch(modalToggle('modal-foobar'))}}>try me</button>
                 <Modal id="modal-foobar" title="react-modal example">
                     <div>
                         <p>Bacon ipsum dolor amet meatloaf porchetta pork loin hamburger tail, prosciutto kevin beef ribs doner sausage short ribs ground round alcatra ribeye fatback. Tail beef turkey tongue landjaeger pork loin pork chop kevin burgdoggen. Drumstick beef ribs filet mignon shankle landjaeger ham. Beef leberkas brisket doner, jowl pancetta turducken capicola fatback hamburger salami prosciutto chuck tri-tip beef ribs. Jowl short ribs cupim porchetta cow burgdoggen.</p>
